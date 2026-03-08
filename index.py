77f4e6cc680af08fa857001d17b9c686684986c8
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-08 18:28:01 +0530

    Added index.html

---
+++ index.py
@@ -0,0 +1,91 @@
+"""a="harsha"
+b=a[-3]
+print(b)"""
+"""
+
+a=input()
+b=a[::-1]
+
+if a == b:
+    print("palin")
+else:
+    print ("not")"""
+
+"""a, b, c = map(int, input("Enter three numbers (comma separated): ").split(","))
+
+if a > b and a > c:
+    print("Largest:", a)
+
+elif b > a and b > c:
+    print("Largest:", b)
+
+else:
+    print("Largest:", c)"""
+
+
+"""#while loops
+vardhan = 5
+
+while vardhan > 0:
+    print("hello vardhan")
+    vardhan -= 2
+
+print("over")"""
+
+#LOOP for
+
+"""
+ 
+for i in range(2,3):
+    for j in range(1,11):
+        
+        print(f"{i} * {j} = {i*j}")"""
+
+"""
+n=int(input('enter a natural '))
+
+i=1
+
+
+while i<=10 :
+
+   print(f"{n} x {i}={n*i}")
+
+   i += 1
+
+print("over")"""
+
+"""
+n=int(input())
+
+f=1
+
+while n>0 :
+
+     f *= n
+     n -=1
+
+print(f)"""
+
+
+""" 
+ 
+a=input("enter some value :")
+b=str(a)
+
+
+def varible_name(f) :
+    if  "apple" == f :
+        print("10")
+    else:
+        print("20")
+
+varible_name(b)"""
+
+def var(v):
+    if v == 0:      # Base case
+        return
+    print(v)
+    var(v - 1)      # Recursive call
+
+var(4)
---
+++ inp.py
@@ -0,0 +1,93 @@
+"""s=int(input( 
+))
+
+print(type(s))"""
+
+"""
+
+age=int(input("Enter your age :"))
+name=str(input("Enter your name :"))
+
+
+print("hii"+name+"your age ",age)"""
+
+#ERROR HANDLING
+"""
+a=int(input())
+c=int(input())
+print(a,c,sep="h")"""
+
+
+
+"""
+name=str(input())
+
+
+print("hello,"+name,end="!")
+
+"""
+
+
+#split
+"""
+a=input()
+q,e,r=a.split(" ")
+print(q,e,r)
+"""
+
+"""
+a,s=input("enter two values :").split(",")
+e=int(s)
+w=int(a)
+print(e+w)"""
+
+#find area
+
+"""a=int(input("enter radius :"))
+b=3.14*(5**2)
+#c=b**2
+print(b)"""
+
+
+#find roots
+"""
+a=int(input("enter a val :"))
+b=int(input("enter b val :"))
+c=int(input("enter c val :"))
+
+
+n=(b**2)-4*a*c
+
+r1=(-b+(n**2)) / 2*a
+r2=(-b-(n**2)) / 2*a
+
+
+print(r1,r2)"""
+
+#swapping
+
+
+"""
+a=int(input("enter a val :"))
+b=int(input("enter b val :"))
+
+ tem=a
+a=b
+
+b=te 
+
+a=b+a
+
+b=a-b
+
+
+
+
+print(a,b)
+
+ 
+
+
+#if
+
+
